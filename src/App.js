a31c082892067709d5a538c11dd6b08a26fd121a
MatFoxDie <matheus_camacho27@hotmail.com>
2024-03-30 14:51:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Banner from './Componentes/Banner/Banner';
+import Formulario from './Componentes/Formulario';
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+    </div>
+  );
+}
+
+export default App;
